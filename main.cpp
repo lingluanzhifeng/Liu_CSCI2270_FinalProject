be07511cbeb51e415c6bf197b1a21e6b914c4cf8
user <yuli9694@colorado.edu>
2015-04-23 14:45:23 -0600

    New Driver File

--- main.cpp
+++ main.cpp
@@ -7,11 +7,17 @@ int main()
     Restaurant r;
     r.initial_all();
     r.print_all_restaurants();
-    cout << endl;
+
     r.print_one_type_restaurants("Pizza");
-    cout << endl;
+
     r.print_one_city_restaurants("Boulder");
-    cout << endl;
+
     r.print_nearest_restaurants("Boulder");
+
+    r.print_minimum_cost_each_restaurants(3, 8, "Boulder");
+
+    r.print_total_cost_one_restaurant("Five Guys", "Broomfield", 3, 8);
+
+    r.find_the_minimum_cost(3, 8, "Boulder");
     return 0;
 }
