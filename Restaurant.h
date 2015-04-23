e206ada5070a3a4e4861ed1721a9c25eb3fd3a71
user <yuli9694@colorado.edu>
2015-04-23 14:45:53 -0600

    New Header File

--- Restaurant.h
+++ Restaurant.h
@@ -6,12 +6,12 @@ struct vertex;
 
 struct adjVertex{//struct for adjacent cities
     vertex *v;
-    int weight;
+    double weight;
 };
 
 struct vertex{ // struct for each city
     std::string name;
-    int distance_to_origin;
+    double distance_to_origin;
     int ID;
     bool visited;
     std::vector<adjVertex> adj;
@@ -19,17 +19,17 @@ struct vertex{ // struct for each city
 
 struct queueVertex{
     int p[6];
-    int distance; //number of edges on pathvoid print_shortest_distance (queueVertex *x);
+    double distance; //number of edges on pathvoid print_shortest_distance (queueVertex *x);
     std::vector <vertex> path; //vertices on the path
 };
 
 struct Restaurant_str{//struct for restaurants
-    int price;
+    double price;
     std::string type;
     std::string city;
     std::string name;
     Restaurant_str(){};
-    Restaurant_str (int in_price, std::string in_type, std::string in_city, std::string in_name){
+    Restaurant_str (double in_price, std::string in_type, std::string in_city, std::string in_name){
         type = in_type;
         name = in_name;
         city = in_city;
@@ -42,23 +42,22 @@ class Restaurant
     public:
         Restaurant();
         ~Restaurant();
+        void print_menu();
         void initial_all();//implement cities and restaurants
         void implement_graph();
         void implement_restaurants();
-        void insert_restaurant(std::string in_type, std::string in_city, std::string in_name, int in_price);//for users to add their restaurants
-        void addEdge(std::string v1, std::string v2, int weight);
+        void insert_restaurant(std::string in_type, std::string in_city, std::string in_name, double in_price);//for users to add their restaurants
+        void addEdge(std::string v1, std::string v2, double weight);
         void addVertex(std::string name);
         void print_all_restaurants();//print all of the restaurants
         void print_one_type_restaurants(std::string type);//print the restaurants which users choose
         void print_one_city_restaurants(std::string city);//print restaurants in one city
         void print_nearest_restaurants(std::string position);//print restaurants near the user's city
-
-        void print_minimum_cost_each_restaurants();//print the minimum costs for each restaurants
-        void print_total_cost_one_restaurant(std::string name);//print the cost for one restaurant
-
-        void displayEdges();
+        void print_minimum_cost_each_restaurants(double price, double mile, std::string position);//print the minimum costs for each restaurants, take the oil price and miles/gallon as parameters
+        void print_total_cost_one_restaurant(std::string name, std::string position, double price, double mile);//print the cost for one restaurant
         queueVertex *Dijkstra_algorithm (std::string startingcity, std::string destination);
         void print_shortest_distance (queueVertex *x, std::string startingcity, std::string destination);
+        void find_the_minimum_cost (double price, double mile, std::string position);//find the minimum cost among all of the restaurants
 
     protected:
     private:
